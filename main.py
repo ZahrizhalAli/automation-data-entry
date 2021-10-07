1d6e2453d97a88ff0d2d6017b0c88ed9a18606d7
zahrizhalali29 <zzahrizhal.ali@gmail.com>
2021-10-07 13:48:58 +0800

    Siap pake

---
+++ main.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0",
+    "Accept-Language": "en-US,en;q=0.5"
+}
+response = requests.get(url="https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.79725111914063%2C%22east%22%3A-122.06940688085938%2C%22south%22%3A37.51885724965468%2C%22north%22%3A38.03083927665764%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D", headers=headers)
+data = response.text
+
+# print(data)
+soup = BeautifulSoup(data, "html.parser")
+all_selected_items =  soup.select(".list-card-top a")
+
+all_links = []
+for link in all_selected_items:
+    href = link['href']
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+
+all_addresses_items = soup.select(".list-card-info address")
+all_address = [address.getText() for address in all_addresses_items]
+
+all_prices_list = soup.select(".list-card-info .list-card-price")
+all_prices = [prices.getText() for prices in all_prices_list]
+
+
+chrome_driver_path = "C:\Development\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get("https://forms.gle/HLnmm8t7LRkfs3s6A")
+
+time.sleep(2)
+prices_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+address_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+link_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+
+for n in range(len(all_prices)):
+    time.sleep(1)
+    prices_input.send_keys(all_prices[n])
+    time.sleep(1)
+    address_input.send_keys(all_address[n])
+    time.sleep(1)
+    link_input.send_keys(all_links[n])
+    time.sleep(1)
+    btn_submit =driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+    btn_submit.click()
+    time.sleep(3)
+    back_link = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+    back_link.click()
